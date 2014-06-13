9831f81aba7717173d5ced7e98379dad2d44873d
JuanREmb <juan.rodriguez@embarcadero.com>
2014-06-13 12:07:49 +0200

    Adding controls (testing commit)

--- Unit4.pas
+++ Unit4.pas
@@ -4,14 +4,15 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls;
 
 type
   TForm4 = class(TForm)
   private
     { Private declarations }
   public
-    { Public declarations }
+
+    Button1: TButton;   { Public declarations }
   end;
 
 var
